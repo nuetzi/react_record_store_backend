d3ea7bbe95c443f6a4d150256a409bd1dec6b8e2
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-04-06 22:11:11 -0400

    Made Schema require all properties

--- models/records.js
+++ models/records.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const recordSchema = new mongoose.Schema({
-    title: String,
-    artist: String,
-    year: String,
-    genre: String,
-    image: String
+    title: {type: String, required: true},
+    artist: {type: String, required: true},
+    year: {type: Number, required: true},
+    genre: {type: String, required: true},
+    image: {type: String, required: true}
 });
 
 const Records = mongoose.model("Record", recordSchema);
--- server.js
+++ server.js
@@ -22,7 +22,6 @@ db.on("disconnected", () => console.log("Mongo disconnected"));
 // Middleware
 app.use(express.urlencoded({ extended:false }));
 app.use(express.json());
-app.use(express.static("public"));
 app.use(cors());
 
 // Routes
